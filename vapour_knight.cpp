9cce4a3508b776d1535f9a68c280c927a8af6a6b
Sleepy Gary Oak <VapourKnight@protonmail.com>
2019-08-14 15:40:07 -0400

    added files

---
+++ vapour_knight.cpp
@@ -0,0 +1,72 @@
+// Celestial Digital Life Gym, Soular Powered RN JesuIs
+// Sacred Sword Algebra Trainer
+
+#include <stdio.h>      /* printf */
+#include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
+#include <math.h>       /* sqrt */
+
+#include <iostream>
+#include <string>
+#include <deque>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void prayer_printer(){
+	for (int i = 0; i <= 999; i++){
+		cout << i <<  " Gloria in Excelsis Deo!" << endl;
+	}
+	return;
+}
+
+int main() {
+	//33 random n-size {LEFT ARM TO REST} [Za Warudo]
+	vector<string> deck;	
+
+	//16 n-size {CHRISTOS TO BE} [Ora Muda Card Clash]
+
+	//1-size {RIGHT ARM TO MOTION} [Hermit Purple]
+
+	//Clock Counter
+	clock_t t;
+  	int f;
+  	t = clock();
+  	printf ("Calculating...\n");
+  	prayer_printer();
+  	t = clock() - t;
+  	printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+
+	return 0;
+}
+
+
+
+/*
+Random Notepad Diary
+
+turn all numbers into prayers that focus on fruits of the spirit, gifts of the spirit,
+art, adventures, games, archetypes, tree of life, bright tarot cards, etc..
+
+	How to become what you seed: https://www.youtube.com/watch?v=V0BOXGv50Dw
+	
+	Aspects of Divinity: (Infinite Power, Zero Force)
+	Infinite Mercy
+	Infinite Love
+	Zero Fear
+	Infinite Courage
+	Zero Cowardice
+	Infinite Evolution of Consciousness
+	Zero Stagnation
+	Infinite Fun
+	Infinite Creativity
+	Infinite Adventure
+	Zero Boredeom
+	Zero Suffering
+	Infinite Patience
+	Infinite Gentleness
+	All Life is Equally Sacred
+
+
+
+*/
